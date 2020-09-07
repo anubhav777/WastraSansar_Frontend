1dc7e5417eb016365b6b5947fc21a05e676a6392
Anubhav Thapa <magaranub@gmail.com>
2020-09-07 07:05:45 +0545

    Loginpop added

---
+++ src/components/Users/Loginpop.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import Popup from 'reactjs-popup'
+import {Redirect} from 'react-router-dom'
+import {Formik, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
+import axios from 'axios'
+import '../../App.css'
+import show_noty from './Notify'
+class Loginpop extends Component {
+    state = {
+        email:'',
+        password:'',
+        redirect:false
+    }
+
+    render() {
+        return (
+
+           
+                <Popup  trigger={<a  >Login</a>} modal>
+            {close => (
+              <div className="pope">
+         
+
+
+    <div className="container">
+    <input type="image" id="imgbtn" style={{width:'32px',height:'32px', position:'absolute',marginLeft:'-25px',zIndex: 2,marginTop: '-10px'}}  src="https://image.flaticon.com/icons/png/512/106/106830.png"  alt="Tool Tip" onClick={close}/>
+    <div className="card" style={{background:'#fafafa'}}><Formik initialValues={this.state} validationSchema={Yup.object().shape({
+                       
+                       
+                        email:Yup.string()
+                        .email("Email is required")
+                        .required("Email is required for registration"),
+
+                        password:Yup.string()
+                        .required("please enter a password")
+                        .matches(/^(?=.*[0-9])(?=.*[a-z])(?=.{8,})/,
+                        "Must Contain 8 Characters,contain One Number and One alphabet at minimum"),
+
+                    })  } onSubmit={(values,err)=>{
+                     
+                        let email = values.email
+                        let password = values.password
+                        console.log(email,password)
+                        let data={
+                            "email":email,
+                            "password":password
+                        }
+                        axios.post(' http://127.0.0.1:8000/api/token/',data,{
+                            headers:{
+                                "Content-Type":"application/json"
+                            }
+                        })
+                        .then(res=>{
+                            console.log(res)
+                            if (res.data.status === 'success'){
+                                console.log(res.data.access)
+                                localStorage.setItem('Token',res.data.access)
+                                let fg=document.getElementById('imgbtn')
+                                  
+                                        setTimeout(()=>{fg.click()},500)
+                             
+                            }
+                            else{
+                              show_noty('error','Your credentials do not match')
+                            }
+                            if (res.data.usert === 'admin'){
+                              localStorage.setItem('Usert',res.data.usert)
+                            }
+                           
+                        })
+
+                    }} >
+                        {({values,handleChange,handleBlur,handleSubmit})=>(
+
+      <form className="form-horizontal" onSubmit={handleSubmit}>
+        <div className="card-body">
+          <h4 className="card-title" style={{float:'left',position:'absolute'}}>Login</h4><br/>
+         
+          <div className="form-group row">
+          
+            <label htmlFor="lname" className="col-sm-3 text-right control-label col-form-label">Email</label>
+            <div className="col-sm-6">
+            <ErrorMessage component="div" name="email" style={err}/>
+              <input type="text"  className="form-control" name="email" placeholder="Email Name Here"  required value={values.name} onChange={handleChange} onBlur={handleBlur}/>
+            </div>
+          </div>
+          <div className="form-group row">
+          
+            <label htmlFor="lname" className="col-sm-3 text-right control-label col-form-label">Password</label>
+            <div className="col-sm-6">
+            <ErrorMessage component="div" name="password" style={err}/>
+              <input type="password"  className="form-control" name="password" placeholder="Password Here" required value={values.name} onChange={handleChange} onBlur={handleBlur} />
+            </div>
+          </div>
+          
+        </div>
+        <div className="border-top">
+          <div className="card-body">
+            <button type="submit" className="btn btn-success" style={{float:'left'}}  >login</button>
+          </div>
+        </div>
+      </form>
+       )}
+                   
+       </Formik>
+    </div>
+    </div>
+
+
+</div>
+
+                
+              )}
+            </Popup>
+    
+          
+        );
+    }
+}
+const fn={
+    fontFamily: 'Graduate',fontSize: '16px'
+  }
+  const ml={
+    marginLeft:'15px',width:'150px',textAlign: 'center'
+   
+  }
+const anch={
+    fontSize: '12px',
+    color: '#666666',
+    position: 'relative',
+    marginRight: '8px',
+
+
+}
+const err={
+  color:"red"
+}
+export default Loginpop;
